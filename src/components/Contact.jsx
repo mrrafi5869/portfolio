bf2cc43defc4a64191e1e80378cd132d4d66f4e4
Mohammad Rafe <mrrafi5869@gmail.com>
2023-12-04 11:52:34 +0600

    sdf

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,8 @@ import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 import contact from "./contact.json";
 import { styles } from "../styles";
-import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
-import Lottie from "react-lottie";
 
 const Contact = () => {
   const defaultOptions = {
